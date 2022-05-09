2eafb120bc0aee2e8b6cfdbcaf6d6e78489b90d6
Dawid <65316603+xStck@users.noreply.github.com>
2022-05-09 08:51:13 +0200

    WorkingClientServerLab8

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const connection = require('./db')
-c
+
 const userRoutes = require("./routes/users")
 const authRoutes = require("./routes/auth")
 
